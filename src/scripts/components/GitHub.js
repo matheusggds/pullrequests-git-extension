ee21e7103ded767a209a17271577357709c2fd45
Matheus Gomide <matheusggds@hotmail.com>
2019-01-11 17:03:36 -0200

    github login

--- src/scripts/components/GitHub.js
+++ src/scripts/components/GitHub.js
@@ -3,21 +3,25 @@ import React, {Component} from 'react'
 export default class GitHub extends Component {
   render() {
 	return (
-		<div className="container">
-			<div className="jumbotron">
-				<h1>GitHub Info</h1>
-				<form className="form-login">
-					<div className="form-group">
-						<label for="login_username">Usuário</label>
-						<input type="text" className="form-control" name="login_username" id="login_username" aria-describedby="GitHub Username"/>>
-						<small id="helpId" className="form-text text-muted">Usuário do GitHub</small>
+		<div className="login">
+			<div className="container">
+				<h1 className="login__title">Login GitHub</h1>
+				<hr/>
+				<div className="row justify-content-center">
+					<div className="col-6">
+						<form className="form-login">
+							<div className="form-group">
+								<label for="login_username">Usuario</label>
+								<input type="text" className="form-control" name="login_username" id="login_username" aria-describedby="GitHub Username"/>
+							</div>
+							<div className="form-group">
+								<label for="login_password">Senha</label>
+								<input type="password" className="form-control" name="login_password" id="login_password" aria-describedby="GitHub Password"/>
+							</div>
+							<button type="submit" className="btn btn-primary">Autenticar</button>
+						</form>
 					</div>
-					<div className="form-group">
-						<label for="login_password">Senha</label>
-						<input type="password" className="form-control" name="login_password" id="login_password" aria-describedby="GitHub Password"/>
-					</div>
-					<button type="submit" className="btn btn-primary">Autenticar</button>
-				</form>
+				</div>
 			</div>
 		</div>
 	)
