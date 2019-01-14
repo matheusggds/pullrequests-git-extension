aeca08b8c2927d9cbe7923bb59dee76b46388a46
Matheus Gomide <matheusggds@hotmail.com>
2019-01-14 14:58:07 -0200

    last updated and panels structure

--- src/scripts/components/Dashboard.js
+++ src/scripts/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GitHub from 'github-api';
+import Panel from './Panel';
 
 export default class Dashboard extends Component {
 	constructor(props) {
@@ -18,7 +19,26 @@ export default class Dashboard extends Component {
 					<span className="dashboard__welcome-msg">
 						Bem vindo, {this.props.user.name}
 					</span>
+					<span className="dashboard__last-update -js-update">
+						14/01 14:48 <i className="fas fa-bullseye"></i>
+					</span>
 				</header>
+				<section className="dashboard__panels">
+					<div className="container">
+						<div className="row">
+							<div className="col-6">
+								<div className="dashboard__panel">
+									<Panel/>
+								</div>
+							</div>
+							<div className="col-6">
+								<div className="dashboard__panel">
+									<Panel/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
 			</div>
 		)
 	}
