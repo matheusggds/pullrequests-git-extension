238265b90dfe1a9334d8fd89ca900c0f8d4e90db
Matheus Gomide <matheusggds@hotmail.com>
2019-01-14 14:57:35 -0200

    panel component

---
+++ src/scripts/components/Panel.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import GitHub from 'github-api';
+
+export default class Panel extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<h1>oi</h1>
+		)
+	}
+}
