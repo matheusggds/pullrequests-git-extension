0de29c04f94f478c5eb298f7248c5f954dc04382
Matheus Gomide <matheusggds@hotmail.com>
2019-01-15 14:04:12 -0200

    new at react, sorry, to mutch things again, <3

--- src/scripts/components/Dashboard.js
+++ src/scripts/components/Dashboard.js
@@ -5,7 +5,26 @@ import Panel from './Panel';
 export default class Dashboard extends Component {
 	constructor(props) {
 		super(props);
+
 		document.body.className += ' ' + 'dashboard';
+
+		this.labels = ['waiting-merge', 'waiting-adjust', 'review-required']
+
+		/* Create state obj */
+		this.state = {}
+
+		/* Set default state labels to 0 before Ajax to update */
+		this.labels.map(el => {
+			this.state[el] = 0
+		})
+
+		this.handlerState = this.handlerState.bind(this);
+	}
+
+	handlerState (key, value) {
+		this.setState({
+			[key]: value
+		});
 	}
 
 	render() {
@@ -33,28 +52,56 @@ export default class Dashboard extends Component {
 					</li>
 					<li className="label-description__item">
 						<span className="label-description__color label-description__color--approved"></span>
-						<span className="label-description__desc">Approved</span>
-					</li>
-					<li className="label-description__item">
-						<span className="label-description__color label-description__color--waitingmerge"></span>
-						<span className="label-description__desc">Waiting merge</span>
+						<span className="label-description__desc">Waiting Merge</span>
 					</li>
 				</ul>
 				<section className="dashboard__panels">
 					<div className="row">
 						<div className="col-6">
-							<Panel name="Brastemp" shortname="btp" gh={this.props.gh} repo="brastemp-vtex"/>
+							<Panel
+								name="Brastemp"
+								shortname="btp"
+								gh={this.props.gh}
+								repo="brastemp-vtex"
+								parentState={this.state}
+								labels={this.labels}
+								handlerState={this.handlerState}
+							/>
 						</div>
 						<div className="col-6">
-							<Panel name="Consul" shortname="cns" gh={this.props.gh} repo="consul-vtex"/>
+							<Panel
+								name="Consul"
+								shortname="cns"
+								gh={this.props.gh}
+								repo="consul-vtex"
+								parentState={this.state}
+								labels={this.labels}
+								handlerState={this.handlerState}
+							/>
 						</div>
 					</div>
 					<div className="row">
 						<div className="col-6">
-							<Panel name="Direto na Loja" shortname="dnl" gh={this.props.gh} repo="diretonaloja-vtex"/>
+							<Panel
+								name="Direto na Loja"
+								shortname="dnl"
+								gh={this.props.gh}
+								repo="diretonaloja-vtex"
+								parentState={this.state}
+								labels={this.labels}
+								handlerState={this.handlerState}
+							/>
 						</div>
 						<div className="col-6">
-							<Panel name="Compra Certa" shortname="cc" gh={this.props.gh} repo="compra-certa-3-vtex"/>
+							<Panel
+								name="Compra Certa"
+								shortname="cc"
+								gh={this.props.gh}
+								repo="compra-certa-3-vtex"
+								parentState={this.state}
+								labels={this.labels}
+								handlerState={this.handlerState}
+							/>
 						</div>
 					</div>
 				</section>
--- src/scripts/components/Panel.js
+++ src/scripts/components/Panel.js
@@ -5,11 +5,15 @@ export default class Panel extends Component {
 	constructor(props) {
 		super(props);
 
-		this.statusArry = ['approved', 'waiting-merge', 'waiting-adjust', 'review-required'];
-
+		/* Default state */
 		this.state = {
-			loaded: false
+			loaded: false,
 		}
+
+		/* Set default state labels to 0 before Ajax to update */
+		this.props.labels.map(el => {
+			this.state[el] = 0
+		})
 	}
 
 	componentDidMount() {
@@ -22,34 +26,22 @@ export default class Panel extends Component {
 
 		repo.listPullRequests().then(res => {
 
+			/* Loop PR's */
 			res.data.map((el) => {
-				let currentLabel = el.labels[0].name.replace(' ', '-');
+				let currentLabel = el.labels[0].name.replace(' ', '-'); /* Get label name */
 
-				console.log(currentLabel);
 
-				if (this.statusArry.includes(currentLabel)) {
-					this.setState({
-						[currentLabel] : this.state[currentLabel] ? this.state[currentLabel] + 1 : 1
-					});
-				}
+				this.setState({
+					[currentLabel] : this.state[currentLabel] ? this.state[currentLabel] + 1 : 1
+				});
 			});
-
-			/* let filteredData = res.data.map((el) => {
-				return {
-					owner: {
-						avatar: el.user.avatar_url,
-						url: el.user.html_url,
-					},
-					labels: el.labels,
-					link: el.html_url,
-					id: el.number,
-					title: el.title
-				}
-			});
-			 */
 
 			this.setState({
 				loaded: true
+			})
+
+			this.props.labels.map(el => {
+				this.props.handlerState([el], (this.props.parentState[el] ? this.props.parentState[el] : 0) + this.state[el])
 			})
 		})
 	}
@@ -65,37 +57,18 @@ export default class Panel extends Component {
 					</div>
 					<div className="repo-panel__status">
 						<ul className="repo-panel__status-list">
-							<li className="repo-panel__status-item repo-panel__status-item--reviewaquired">{this.state['review-required']}</li>
-							<li className="repo-panel__status-item repo-panel__status-item--waitingadjust">{this.state['waiting-adjust']}</li>
-							<li className="repo-panel__status-item repo-panel__status-item--approved">{this.state['approved']}</li>
-							<li className="repo-panel__status-item repo-panel__status-item--waitingmerge">{this.state['waiting-merge']}</li>
+							<li className="repo-panel__status-item repo-panel__status-item--reviewaquired">
+								{this.state['review-required'] ? this.state['review-required'] : '-'}
+							</li>
+							<li className="repo-panel__status-item repo-panel__status-item--waitingadjust">
+								{this.state['waiting-adjust'] ? this.state['waiting-adjust'] : '-'}
+							</li>
+							<li className="repo-panel__status-item repo-panel__status-item--approved">
+								{this.state['waiting-merge'] ? this.state['waiting-merge'] : '-'}
+							</li>
 						</ul>
 					</div>
 				</div>
-
-				/* <div className={"repo-panel repo-panel--" + this.props.shortname}>
-					<div className="repo-panel__title">
-						{this.props.name}
-					</div>
-					<div className="repo-panel__body">
-						{this.state.pullrequests.map((el, idx) => {
-							return (
-								<div id={el.id} key={el.id} className="repo-panel__item">
-								<span className="repo-panel__thumbavatar">
-									<a href={el.owner.url}>
-										<img src={el.owner.avatar} alt={el.owner.url}/>
-									</a>
-								</span>
-									<span className="repo-panel__pr-title">
-										{el.title}
-									</span>
-									<span className={"repo-panel__label repo-panel__label--" + el.labels[0].name.replace(' ', '-')}>
-									</span>
-								</div>
-							)
-						})}
-					</div>
-				</div> */
 			)
 		}
 	}
